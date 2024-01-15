dc953fb0b2406599cdbb8374c468c0f015c73d85
J Mac <jbatcheller123@gmail.com>
2024-01-15 10:25:31 -0700

    I created the databases for the schema and the seeds. I still need to create the query, and join the tables. That is what I will work on next, just adding this commit to save what I have done so far

---
+++ db/seeds.sql
@@ -0,0 +1,43 @@
+INSERT INTO department (id, name)
+VALUES (1, "Accounting"), 
+       (2, "Sales"),
+       (3, "Engineering"),
+       (4, "Marketing"),
+       (5, "Customer Service"); 
+
+
+INSERT INTO roles (id, title, salary)
+VALUES (6, "Billing Support", 50,000.00), 
+       (7, "Accountant", 80,000.00), 
+       (8, "Sales Manager", 80,000.00), 
+       (9, "Sales Rep", 40,000.00 - 75,000.00), 
+       (10, "Junior Software Engineer", 70,000.00), 
+       (11, "Software Engineer", 100,000.00), 
+       (12, "Lead Software Engineer", 120,000.00), 
+       (13, "Senior Software Engineer", 170,000.00), 
+       (14, "Data Engineer", 75,000.00), 
+       (15, "Data Manager", 100,000.00), 
+       (16, "Marketing Manager", 120,000.00), 
+       (17, "Marketing Coordinator", 85,000.00), 
+       (18, "Custom Service Rep", 45,000.00), 
+       (19, "Technical Support", 55,000.00), 
+       (20, "Customer Success Manager", 80,000.00);
+
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (21, "James", "Batcheller"), 
+       (22, "Jessica", "Sherwood"), 
+       (23, "Benjamin", "Sakas"), 
+       (24, "Garrett", "Soares"), 
+       (25, "Mike", "Penhale"), 
+       (26, "Erica", "Voulle"), 
+       (27, "Corey", "Petersen"), 
+       (28, "Ashley", "Owens"), 
+       (29, "Saint", "Navarez"), 
+       (30, "Shane", "O'Neil"), 
+       (31, "Cory", "Kennedy"), 
+       (32, "Paul", "Rodriguez"), 
+       (33, "Dylan", "Jaeb"), 
+       (34, "Chris", "Cole"), 
+       (35, "Frank", "Reynolds"), 
+       (36, "Ishod", "Wair");
